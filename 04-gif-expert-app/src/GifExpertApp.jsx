11dfdf7fa79e4f732bab987c8ebf5fd291083a67
AnaliaGiachini <analiagiachini@gmail.com>
2025-01-10 20:44:17 -0300

    Chapter 6

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -16,9 +16,7 @@ export const GifExpertApp = () => {
   return (
     <>
       <h1>GifExpertApp</h1>
-
       <AddCategory onNewCategory={onAddCategory} />
-      
       {
         categories.map((category) => (
           <GifGrid key={category} category={category} />
